350b4da703d675a8f911d2657e88ad329fcafe47
Debayan Pradhan <pradhandebayan@gmail.com>
2024-05-10 20:01:51 +0530

    added error handlers and integrated changes

--- src/app.ts
+++ src/app.ts
@@ -8,15 +8,17 @@ if(!process.env.NODE_ENV) {
     })
 }
 
-import express, {Express, Request, Response } from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 import mainRouter from './routes/main.route';
+import { ErrorHandler } from './utils/error.handler';
+import { GenerateResponse } from './utils/response.creator';
 
 const PROJECT_NAME = String(process.env.PROJECT_NAME);
 const BASE_URL = String(process.env.BASE_URL) || "http://127.0.0.1";
 const PORT = Number(process.env.PORT);
 
-const app : Express = express();
+const app : express.Application = express();
 
 app.disable("x-powered-by");
 
@@ -26,6 +28,12 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.use("/api/v1/main", mainRouter);
+
+app.use('*', (req:Request, res : Response) => {
+    GenerateResponse(res, 404);
+});
+
+app.use(ErrorHandler);
 
 app.get('/status',(req:Request, res : Response) => {
     res.send('Server is up and running')
